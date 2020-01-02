308b5b1dd880dfbceac9264dff4be25fa7767779
RD Hawladar <rdhawladar@gmail.com>
2020-01-02 22:57:15 +0600

    Making Restaurant repository from restaurant entity

--- src/Controller/Api/v1/RestaurantController.php
+++ src/Controller/Api/v1/RestaurantController.php
@@ -4,6 +4,7 @@ namespace App\Controller\Api\v1;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\HttpFoundation\Response;
 use FOS\RestBundle\Controller\Annotations as Rest;
 use Symfony\Component\HttpFoundation\Request;
 use App\Entity\Restaurants;
@@ -16,14 +17,16 @@ class RestaurantController extends AbstractController
 {
     /**
      * Lists Restaurants.
-     * @Rest\Get("/restaurant")
+     * @Rest\Get("/restaurants")
      *
      * @return Response
      */
     public function getMovieAction(Request $request)
     {
-        $repository = $this->getDoctrine()->getRepository(Restaurants::class);
-        $restaurants = $repository->findall();
+        $restaurants = $this->getDoctrine()
+            ->getRepository(Restaurants::class)
+            ->findall();
+
         return $this->json([
             'message' => 'Welcome to your new controller!',
             'path' => 'src/Controller/RestaurantController.php',
